eebcd117f202967b6226d5f2a904d5ad914011f8
Jona141999 <jona94alquinga@gmail.com>
2021-01-18 17:55:13 -0500

    tercer script

---
+++ 3.py
@@ -0,0 +1,48 @@
+from facebook_scraper import get_posts
+import couchdb
+import json
+import time
+import pymongo
+import pprint
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import json
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017")    #('http://115.146.93.184:5984/')
+try:
+    db = myclient["prueba3"]
+    col= db["datos"]
+except:
+    db = myclient["prueba3"]
+    col= db["datos"]
+    
+i=1
+for post in get_posts('Fifa',pages=1000,extra_info=True):
+    print(i)
+    i=i+1
+    time.sleep(5)
+    
+    id=post['post_id']
+    doc={}
+    
+    doc['id']=id
+    mydate=post['time']
+    
+    try:
+        doc['texto']=post['text']
+        doc['date']=mydate.timestamp()
+        doc['likes']=post['likes']
+        doc['comments']=post['comments']
+        doc['shares']=post['shares']
+        try:
+            doc['reactions']=post['reactions']
+        except:
+            doc['reactions']={}
+        
+        doc['post_url']=post['post_url']
+        col.save(doc)
+        
+        print('guardado con exito')
+    except Exception as e:
+        print('no se pudo guardar:'+str(e))
